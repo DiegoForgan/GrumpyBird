52dac79f4fb5ba09bd0170a588b051aacd66ce1c
DiegoForgan <dmforgan89@gmail.com>
2021-10-20 18:43:47 -0300

    Player can flap and some UI text done

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float forceMultiplier;
+    private Rigidbody2D playerRB;
+    private Animator playerAnimator;
+
+    private bool shouldFlap = false;
+    
+
+    private void Awake() {
+        playerRB = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        shouldFlap = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !shouldFlap){
+            shouldFlap = true;
+        }
+    }
+
+    private void FixedUpdate() {
+        if (shouldFlap) Flap();
+    }
+
+    private void Flap()
+    {
+        playerRB.velocity = Vector2.zero;
+        playerRB.AddForce(Vector2.up * forceMultiplier,ForceMode2D.Impulse);
+        shouldFlap = false;
+    }
+}
